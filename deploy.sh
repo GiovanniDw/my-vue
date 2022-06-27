d25764ced30876de67a98c12cbaccf9535ceb4a4
“GiovanniDw” <giovannidwidjosewojo@hotmail.com>
2022-06-27 12:23:52 +0200

    update

--- deploy.sh
+++ deploy.sh
@@ -7,21 +7,22 @@ set -e
 yarn run build
 
 # navigate into the build output directory
-# cd docs
+cd dist
 
 # if you are deploying to a custom domain
 # echo 'www.example.com' > CNAME
 
-# git init
+git init
 # git checkout main
-# git add -A
-# git commit -m 'deploy'
+git add -A
+git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git subtree push --prefix dist origin gh-pages
+# git subtree push --prefix dist origin gh-pages
+git push -f git@github.com:giovannidw/my-vue.git main:gh-pages
 
-# cd -
+cd -
 
